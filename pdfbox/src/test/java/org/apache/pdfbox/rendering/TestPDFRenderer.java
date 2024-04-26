eda42568985b20ef8db833f687dc13ae0c823b7f
Larry Lynn <larry.lynn@workiva.com>
2024-04-26 12:19:55 -0600

    got the repro doc processing in PDFBox code

--- pdfbox/src/test/java/org/apache/pdfbox/rendering/TestPDFRenderer.java
+++ pdfbox/src/test/java/org/apache/pdfbox/rendering/TestPDFRenderer.java
@@ -3,20 +3,26 @@ package org.apache.pdfbox.rendering;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.InputStream;
+import java.net.URL;
 
 public class TestPDFRenderer {
 
     public static final String type4_shading_pdf = "minimal.pdf";
 
     @Test
-    public void testGetTotalFilingSize() throws Exception {
-        ClassLoader classloader = Thread.currentThread().getContextClassLoader();
-        String filePath = classloader.getResource(type4_shading_pdf).getFile();
+    public void testConvertType4Shading() throws Exception {
+        InputStream is = this.getClass().getResourceAsStream("/input/rendering/minimal.pdf");
+//        ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+//        URL resource = classloader.getResource("/rendering");
+//        String filePath = resource.getFile();
+//        File inputFile = new File(filePath);
+        String filePath = this.getClass().getResource("/input/rendering/minimal.pdf").getFile();
         File inputFile = new File(filePath);
 
 
-        TestPDFToImage.doTestFile(inputFile, "src/test/resources/input/rendering", "target/test-output/rendering");
+        TestPDFToImage.doTestFile(inputFile, "src/test/resources/input/rendering", "target/test-output");
 
     }
 }
